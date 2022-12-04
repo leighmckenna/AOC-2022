4aaf24b635dadd1283e58cc1c2d1a86882f3f0fa
Leigh McKenna <leigh@leighmckenna.com>
2022-12-04 16:41:57 +1000

    Solve D2P2

--- Day 2/src/main.rs
+++ Day 2/src/main.rs
@@ -1,13 +1,15 @@
 use std::fs;
 use std::io::{BufReader, BufRead};
 
+
 #[derive(Debug, PartialEq)]
 enum Sign {
-    Rock = 1,
-    Paper = 2,
-    Scissors = 3,
+    Rock = 1, // loss
+    Paper = 2, // draw
+    Scissors = 3, // win
 }
 
+// Anguished sounds of incorrect structs
 #[derive(Debug)]
 struct Guide {
     enemy: Sign,
@@ -47,27 +49,27 @@ fn parse_data() -> Vec<Guide> {
 }
 
 // determine who wins in rock paper scissors and calculate the round score
-fn eval_round(guide: Guide) -> i32 {
-    let enemy = guide.enemy;
-    let my = guide.my;
+fn eval_round(guide: &Guide) -> i32 {
+    let enemy = &guide.enemy;
+    let my = &guide.my;
 
     let mut score = 0;
-    if enemy == Sign::Rock {
-        if my == Sign::Paper {
+    if enemy == &Sign::Rock {
+        if my == &Sign::Paper {
             score = 6;
-        } else if my == Sign::Rock {
+        } else if my == &Sign::Rock {
             score = 3;
         }
-    } else if enemy == Sign::Paper {
-        if my == Sign::Scissors {
+    } else if enemy == &Sign::Paper {
+        if my == &Sign::Scissors {
             score = 6;
-        } else if my == Sign::Paper {
+        } else if my == &Sign::Paper {
             score = 3;
         }
-    } else if enemy == Sign::Scissors {
-        if my == Sign::Rock {
+    } else if enemy == &Sign::Scissors {
+        if my == &Sign::Rock {
             score = 6;
-        } else if my == Sign::Scissors {
+        } else if my == &Sign::Scissors {
             score = 3;
         }
     }
@@ -80,14 +82,49 @@ fn eval_round(guide: Guide) -> i32 {
     return score;
 }
 
+fn cheat_round(guide: &Guide) -> i32 {
+    let enemy = &guide.enemy;
+    let my = &guide.my;
+
+    let mut score = 0;
+    match enemy {
+        Sign::Rock => {
+            match my {
+                Sign::Rock => score += 0 + 3, // lose and scissor
+                Sign::Paper => score += 3 + 1, // tie and rock
+                Sign::Scissors => score += 6 + 2, // win and paper
+            }
+        },
+        Sign::Paper => {
+            match my {
+                Sign::Rock => score += 0 + 1, // lose and rock
+                Sign::Paper => score += 3 + 2, // tie and paper
+                Sign::Scissors => score += 6 + 3, // win and scissor
+            }
+        },
+        Sign::Scissors => {
+            match my {
+                Sign::Rock => score += 0 + 2, // lose and paper
+                Sign::Paper => score += 3 + 3, // tie and scissor
+                Sign::Scissors => score += 6 + 1, // win and rock
+            }
+        },
+    }
+
+    return score;
+}
+
 fn main() {
     let guides = parse_data();
 
     let mut total_score = 0;
+    let mut cheat_score = 0;
     for guide in guides {
-        total_score += eval_round(guide);
+        total_score += eval_round(&guide);
+        cheat_score += cheat_round(&guide);
     }
 
     println!("Total score: {}", total_score);
+    println!("Cheat score: {}", cheat_score);
     
 }
